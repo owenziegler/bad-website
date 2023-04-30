e2424ce1237dca207f2fc52af0298133fa9d14ba
Owen Ziegler <odz2@txstate.edu>
2023-04-30 18:27:22 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{backgroundColor: 'limegreen',height:'100vh'}}>
+      <h1 style={{color:'yellow'}}>
+        Hello!
+      </h1>
+      <p>
+        This is an example of a terrible website interface.
+      </p>
+    </div>
+  );
+}
+
+export default App;
