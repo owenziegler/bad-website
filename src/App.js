94858270da3607dd7d6ee07ddd9e3b4b66147e4e
Owen Ziegler <odz2@txstate.edu>
2023-04-30 19:26:23 -0500

    gif

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import logo from './logo.svg';
+import penguin1 from './pg1.gif';
 import './App.css';
 
 function App() {
   return (
     <div>
       <h1 style={{color:'yellow'}}>
-        Hello!
+        PENGUIN CITY
       </h1>
-      <p>
-        This is an example of a terrible website interface.
-      </p>
+      <img src={penguin1} alt='funny penguin gif'/>
+      <h2 className='text'>
+        Interesting facts about penguins:
+      </h2>
+      <ul>
+        <li className='text'>Penguins have four legs.</li>
+        <li className='text'>Penguins are mammals.</li>
+        <li className='text'>Penguins are capable of complex thought.</li>
+        <li className='text'>Penguins are coming for you.</li>
+      </ul>
     </div>
   );
 }
